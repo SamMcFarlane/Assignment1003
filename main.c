fafd07c73d9813abd8d4c9b5c7ffafdcad46423a
Sam McFarlane <c3324713@uon.edu.au>
2019-04-03 00:21:05 +0000

    Basic Caesar Cipher. No I/O yet. Need to work out big key.

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 int main(){
-    char str[200];
-    scanf("%s",str);
-    printf("%s \n",str);
-    return 0;
- }
+    char str[200], code;
+    int i = 0, key = 3;
+    scanf("%s", str);
+    if(key<7){
+        for(i = 0; str[i] != NULL; ++i){
+            code = str[i];
+                if(code>=97 && code<122){
+                    code = code + key;
+                    printf("%c",code);
+                }
+                else if (code>=122){
+                    code = code + key - 25;
+                    printf("%c",code);
+                }
+                if(code>=65 && code<90){
+                    code = code + key;
+                    printf("%c",code);
+                }
+                else if (code>=90 && code<97){
+                    code = code + key - 25;
+                    printf("%c",code);
+                }     
+            }      
+        } else{
+            printf("Change key");
+        }
+}
