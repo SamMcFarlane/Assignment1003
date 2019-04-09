b37f0ff8f909bce9308686b2b4de56b8037d978b
Sam McFarlane <sammcfarlane246@gmail.com>
2019-04-09 23:11:59 +0000

    Capitals --> Key no good

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 void encrypter(char *str, int key);
 int main(){
     char str[200];
-    int key = 3;
+    int key = 5;
     scanf("%s", str);
     encrypter(str, key); 
 }
@@ -10,20 +10,16 @@ void encrypter(char *str, int key){ //where stuff actually gets encrypted
     int i;
         for(i = 0; str[i] != NULL; ++i){
             char code = str[i];
-                if(code>=97 && code<122){
-                    code = code + key;
-                    printf("%c",code);
-                }
-                else if (code>=122){
-                    code = code + key - 25;
-                    printf("%c",code);
-                }
                 if(code>=65 && code<90){
                     code = code + key;
                     printf("%c",code);
                 }
                 else if (code>=90 && code<97){
                     code = code + key - 25;
+                    printf("%c",code);
+                }
+                else if(code>=97 && code<122){
+                    code = code + key - 32;
                     printf("%c",code);
                 }     
             }      
