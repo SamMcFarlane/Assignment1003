86128c9ca080deeece8fdf56a6c6740c14d17447
Sam McFarlane <sammcfarlane246@gmail.com>
2019-04-10 00:20:22 +0000

    Fixed encryption --> Now uses mod arithmetic

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 void encrypter(char *str, int key);
 int main(){
     char str[200];
-    int key = 5;
+    int key = 0;
     scanf("%s", str);
     encrypter(str, key); 
 }
@@ -10,17 +10,31 @@ void encrypter(char *str, int key){ //where stuff actually gets encrypted
     int i;
         for(i = 0; str[i] != NULL; ++i){
             char code = str[i];
-                if(code>=65 && code<90){
-                    code = code + key;
+                code = (code + key)%91;
+                if(code>=65)
+                    printf("%c",code);
+                else if(code<65 && key>0){
+                    code = code + 65;
                     printf("%c",code);
                 }
-                else if (code>=90 && code<97){
-                    code = code + key - 25;
+                else{
+                    code = code + 26;
                     printf("%c",code);
                 }
-                else if(code>=97 && code<122){
-                    code = code + key - 32;
-                    printf("%c",code);
-                }     
+
             }      
         } 
+/*
+#include <stdio.h>
+int main(){
+FILE *input;
+FILE *output;
+input = fopen("test.txt", "r");
+output = fopen("outtest.txt", "w");
+while(!feof(input)){
+    char c;
+    fscanf(input,"%c", &c);
+    fprintf(output,"%c",c);
+}
+}
+*/
